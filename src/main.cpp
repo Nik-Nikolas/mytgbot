a3af1b26eb4165edf4aab383a6fd47d938596f45
Igor <igorlobanoff@gmail.com>
2024-07-12 16:10:51 +0300

    reduce history output

--- src/main.cpp
+++ src/main.cpp
@@ -233,9 +233,9 @@ int main() {
         std::ostringstream ss;
         ss << f.rdbuf();
         auto s = ss.str();
-        const auto max_history_bytes{8192};
+        const auto max_history_bytes{1024};
         if(s.length() > max_history_bytes)
-            s = s.substr(s.length() - max_history_bytes,max_history_bytes);
+            s = s.substr(s.length() - max_history_bytes, max_history_bytes);
         bot1.sayWord(s);
     }});
 
